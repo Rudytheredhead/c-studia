21e18dbe0f78733dbd2ceb71ba1b843be48146d7
Rudytheredhead <patrykpodwojcic06@gmail.com>
2026-04-15 17:38:06 +0200

    aaaaa

---
+++ sem4/lab4/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+double STALA = 1;
+double Dt  = 0.1;
+int DLUGOSC = 10;
+
+void obliczenie_temp(std::vector<std::vector<int>>& plansza){
+    std::vector<std::vector<int>>& plansza_kopia = plansza;
+        for (int x = 0; x<DLUGOSC;x++){
+            for(int y = 0; y<DLUGOSC;y++){
+                double suma = 0.0;
+                for (int dx = -1; dx<=1; dx++){
+                    for (int dy = -1; dy<=1; dy++){
+                        if  (dy == dx or dy == -dx){continue;}
+                        if (x+dx <0 || x+dx >9 || y +dy <0 || y+dy >9){continue;}
+                        suma+= plansza_kopia[x][y] - plansza_kopia[x+dx][y+dy];
+            
+
+                    }
+                }
+                plansza[x][y] += -STALA*Dt*suma;
+    }}
+};
+
+int main(){
+    std::vector<std::vector<int>> plansza(10, std::vector<int>(10,0));
+    plansza[4][5] =500;
+    for (int krok = 0; krok < 10;krok++){
+
+        for (std::vector v:plansza){
+            for (int liczba :v){
+                std::cout<<liczba<<" ";
+            }
+            std::cout<<std::endl;
+        }
+        obliczenie_temp(plansza);
+        std::cout<<std::endl<<"--------------------------------"<<std::endl;
+    }
+    return 0;
+
+};
